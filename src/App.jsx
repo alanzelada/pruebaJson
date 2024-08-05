01d246c8b092ee30b5425f450631ab8ad14bb37e
alanzelada <alanzelada9@gmail.com>
2024-08-05 20:41:59 -0300

    Home created

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css';
 import Header from './components/Header/Header.jsx'
 import Body from './components/Body/Body.jsx'
 import Info from './components/Info/Info.jsx'
+import Filas from './components/Filas/Filas.jsx'
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Info/>
+      <Filas/>
       <Body/>
     </div>
   );
--- src/components/Filas/Filas.jsx
+++ src/components/Filas/Filas.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import '../Filas/Filas.css'
+
+const requireContext = require.context('../../assets/pastillas', false, /\.(png|jpe?g|svg|bmp)$/);
+const images = requireContext.keys().map(requireContext);
 
 const Filas = () => {
   return (
-    <div>Filas</div>
-  )
+    <div className='filasContainer'>
+      <div className="pastillasGroup">
+        {images.map((src, index) => (
+          <img key={index} src={src} alt={`pastilla-${index}`} className='pastillas'/>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Filas
+export default Filas;
