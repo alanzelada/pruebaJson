24a259e159fe3e37dad7a5b62fde4f3fde6c735a
alanzelada <alanzelada9@gmail.com>
2024-08-05 21:25:09 -0300

    finding true and false pastillas

--- src/components/Filas/Filas.jsx
+++ src/components/Filas/Filas.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
-import '../Filas/Filas.css'
+import '../Filas/Filas.css';
+import data from '../../data.json';
 
 const requireContext = require.context('../../assets/pastillas', false, /\.(png|jpe?g|svg|bmp)$/);
 const images = requireContext.keys().map(requireContext);
 
 const Filas = () => {
+  const resultados = data[0]?.resultados;
+
+  if (!Array.isArray(resultados)) {
+    console.error('resultados no está definido o no es un array');
+    return <div>Error al cargar los resultados</div>;
+  }
+
   return (
     <div className='filasContainer'>
       <div className="pastillasGroup">
         {images.map((src, index) => (
-          <img key={index} src={src} alt={`pastilla-${index}`} className='pastillas'/>
+          <img 
+            key={index} 
+            src={src} 
+            alt={`pastilla-${index}`} 
+            className={`pastillas ${resultados[index] === false ? 'bordeRojo' : 'bordeVerde'}`}
+          />
         ))}
+      </div>
+
+      <div className="stats">
+        <div className="statsBandeja"></div>
+        <div className="statsLote"></div>
       </div>
     </div>
   );
