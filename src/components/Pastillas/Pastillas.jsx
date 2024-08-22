7fbb44881a343b1257440f9933dc02463c835039
alanzelada <alanzelada9@gmail.com>
2024-08-22 16:36:35 -0300

    errors matched with pastillas

--- src/components/Pastillas/Pastillas.jsx
+++ src/components/Pastillas/Pastillas.jsx
@@ -1,56 +1,76 @@
-// src/components/Pastillas/Pastillas.jsx
 import React from 'react';
 import getPastillasData from '../../hooks/usePastillasData';
 import '../Pastillas/Pastillas.css';
 
 const Pastillas = () => {
-  const { images, resultados, error } = getPastillasData();
+  const { images, resultados, causa_rechazo, error } = getPastillasData();
 
   if (error) {
     return <div>{error}</div>;
   }
 
-  const resultadosConIndices = resultados.map((resultado, index) => ({ resultado, index }));
+  // Mapeo de los resultados junto con sus índices y causas de rechazo
+  const pastillasConDetalles = resultados.map((resultado, index) => ({
+    resultado,
+    index,
+    causa: causa_rechazo[index] || [], // Asociar cada causa de rechazo con su respectivo índice
+  }));
 
-  const pastillasRojas = resultadosConIndices.filter(item => item.resultado === false);
-  const pastillasVerdes = resultadosConIndices.filter(item => item.resultado === true);
+  const pastillasRojas = pastillasConDetalles.filter(item => item.resultado === false);
+  const pastillasVerdes = pastillasConDetalles.filter(item => item.resultado === true);
 
   return (
     <div className='pastillasContainer'>
       <div className="pastillasRojas">
         <h1>PASTILLAS DEFECTUOSAS</h1>
         <div className="scrollPastillas">
-        {pastillasRojas.map(({ index }) => (
-          <div key={index} className="detallePastilla">
-            <img 
-              src={images[index]} 
-              alt={`pastilla-roja-${index}`} 
-              className='pastillas bordeRojo'
-            />
-            <div className="detalles">
-              <h2>N°: {index+1}</h2>
-              <h2>DEFECTO: </h2>
+          {pastillasRojas.map(({ index, causa }) => (
+            <div key={index} className="detallePastilla">
+              <img 
+                src={images[index]} 
+                alt={`pastilla-roja-${index}`} 
+                className='pastillas bordeRojo'
+              />
+              <div className="detalles">
+                <h2>N°: {index + 1}</h2>
+                <div className="bottom">
+                <h2>DEFECTO{causa.length > 1 ? 'S' : ''}:</h2>
+                <ul>
+                  {causa.map((defecto, idx) => (
+                    <li key={idx}>{defecto}</li>
+                  ))}
+                </ul>
+                </div>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
         </div>
       </div>
       <div className="pastillasVerdes">
         <h1>PASTILLAS APROBADAS</h1>
         <div className="scrollPastillas">
-        {pastillasVerdes.map(({ index }) => (
-          <div key={index} className="detallePastilla">
-            <img 
-              src={images[index]} 
-              alt={`pastilla-verde-${index}`} 
-              className='pastillas bordeVerde'
-            />
-            <div className="detalles">
-              <h2>N°: {index+1}</h2>
-              <h2>DEFECTO: </h2>
+          {pastillasVerdes.map(({ index, causa }) => (
+            <div key={index} className="detallePastilla">
+              <img 
+                src={images[index]} 
+                alt={`pastilla-verde-${index}`} 
+                className='pastillas bordeVerde'
+              />
+              <div className="detalles">
+                <h2>N°: {index + 1}</h2>
+                {/* <h2>DEFECTO{causa.length > 1 ? 'S' : ''}:</h2>
+                <ul>
+                  {causa.length > 0 ? (
+                    causa.map((defecto, idx) => (
+                      <li key={idx}>{defecto}</li>
+                    ))
+                  ) : (
+                    <li>Ninguno</li>
+                  )}
+                </ul> */}
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
         </div>
       </div>
     </div>
--- src/hooks/usePastillasData.js
+++ src/hooks/usePastillasData.js
@@ -5,13 +5,14 @@ const images = requireContext.keys().map(requireContext);
 
 const getPastillasData = () => {
   const resultados = data[0]?.resultados;
+  const causa_rechazo = data[0]?.causa_rechazo;
 
   if (!Array.isArray(resultados)) {
     console.error('resultados no está definido o no es un array');
     return { error: 'Error al cargar los resultados' };
   }
 
-  return { images, resultados };
+  return { images, resultados, causa_rechazo };
 }
 
 export default getPastillasData;
