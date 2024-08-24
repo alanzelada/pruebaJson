4be58e141125a7785908ce9f70d753c49124e3c4
alanzelada <alanzelada9@gmail.com>
2024-08-24 15:15:52 -0300

    stats added and charts in progress

--- src/components/Filas/Filas.jsx
+++ src/components/Filas/Filas.jsx
@@ -2,13 +2,21 @@
 import React from 'react';
 import '../Filas/Filas.css';
 import getPastillasData from '../../hooks/usePastillasData';
+import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 
 const Filas = () => {
-  const { images, resultados, error } = getPastillasData();
+  const { images, resultados, est_bandeja, est_lote, error } = getPastillasData();
 
   if (error) {
     return <div>{error}</div>;
   }
+
+  const data = [
+    { name: "Group A", value: est_bandeja[0] },
+    { name: "Group B", value: 100 - est_bandeja[0] }
+  ];
+
+  const COLORS = ['#FFFFFF', '#FFFFFF33'];
 
   return (
     <div className='filasContainer'>
@@ -24,8 +32,69 @@ const Filas = () => {
       </div>
 
       <div className="stats">
-        <div className="statsBandeja"></div>
-        <div className="statsLote"></div>
+        <div className="statsBandeja">
+          <h2 className='tituloBandeja'>ESTADÍSTICA BANDEJA</h2>
+
+          <div className="bandeja1">
+            <ResponsiveContainer width="100%" height={200}>
+              <PieChart>
+
+                <Pie 
+                  data={data} 
+                  dataKey="value" 
+                  outerRadius={40}
+                  innerRadius={30}
+                  fill="#8884d8"
+                >
+                  {data.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                  ))}
+                </Pie>
+              </PieChart>
+            </ResponsiveContainer>
+            <p>{est_bandeja[0]}</p>
+            <p>END-CAPPING</p>
+          </div>
+
+          <div className="bandeja2">
+            <p>{est_bandeja[1]}</p>
+            <p>DESP-LATERAL</p>
+          </div>
+
+          <div className="bandeja3">
+            <p>{est_bandeja[2]}</p>
+            <p>DESP-SUPERFICIAL</p>
+          </div>
+
+          <div className="bandeja4">
+            <p>{est_bandeja[3]}</p>
+            <p>DEFECTUOSAS</p>
+          </div>
+        </div>
+
+        <div className="statsLote">
+          <h2 className='tituloLote'>ESTADÍSTICA LOTE</h2>
+
+          <div className="lote1">
+            <p>{est_lote[0]}</p>
+            <p>END-CAPPING</p>
+          </div>
+
+          <div className="lote2">
+            <p>{est_lote[1]}</p>
+            <p>DESP-LATERAL</p>
+          </div>
+
+          <div className="lote3">
+            <p>{est_lote[2]}</p>
+            <p>DESP-SUPERFICIAL</p>
+          </div>
+
+          <div className="lote4">
+            <p>{est_lote[3]}</p>
+            <p>DEFECTUOSAS</p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/hooks/usePastillasData.js
+++ src/hooks/usePastillasData.js
@@ -6,13 +6,15 @@ const images = requireContext.keys().map(requireContext);
 const getPastillasData = () => {
   const resultados = data[0]?.resultados;
   const causa_rechazo = data[0]?.causa_rechazo;
+  const est_bandeja = data[0]?.est_bandeja;
+  const est_lote = data[0]?.est_lote;
 
   if (!Array.isArray(resultados)) {
     console.error('resultados no está definido o no es un array');
     return { error: 'Error al cargar los resultados' };
   }
 
-  return { images, resultados, causa_rechazo };
+  return { images, resultados, causa_rechazo, est_bandeja, est_lote };
 }
 
 export default getPastillasData;
